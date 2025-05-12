a76c91233df51107f2e8bb712097e16e4ec241ad
Het20000 <hp985383@gmail.com>
2025-05-12 14:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,39 +9,15 @@ function convertToRoman(num) {
     6: ['I', 1]
   };
 
-  const subtractive = {
-    900: 'CM',
-    400: 'CD',
-    90: 'XC',
-    40: 'XL',
-    9: 'IX',
-    4: 'IV'
-  };
-
-  let ans = "";
-
-  // Add subtractive cases first
-  for (let value of Object.keys(subtractive).sort((a, b) => b - a)) {
-    while (num >= parseInt(value)) {
-      ans += subtractive[value];
-      num -= value;
-    }
-  }
-
-  // Add remaining Roman numerals from obj
-  while (num !== 0) {
-    for (let i in obj) {
-      let symbol = obj[i][0];
-      let value = obj[i][1];
-
-      if (value <= num) {
-        ans += symbol;
-        num -= value;
-        break;
-      }
-    }
-  }
-
-  return ans;
+	let ans = "";
+while (num !==0){
+	for (let i in romanMap) {
+		let [symbol, value] = romanMap[i];
+		if (value <= num) {
+			ans += symbol;
+			num -= value;
+			break;
+		}
+	}
 }
 
