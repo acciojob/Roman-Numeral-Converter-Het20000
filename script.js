808f267c7a149cbc532ac797e9a8a2e2e421f804
Het20000 <hp985383@gmail.com>
2025-05-12 13:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 function convertToRoman(num) {
-  const romanMap = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
+
+  const subtractive = {
+    900: 'CM',
+    400: 'CD',
+    90: 'XC',
+    40: 'XL',
+    9: 'IX',
+    4: 'IV'
+  };
 
   let ans = "";
-  for (let i = 0; i < romanMap.length; i++) {
-    const [roman, value] = romanMap[i];
-    while (num >= value) {
-      ans += roman;
+
+  // Add subtractive cases first
+  for (let value of Object.keys(subtractive).sort((a, b) => b - a)) {
+    while (num >= parseInt(value)) {
+      ans += subtractive[value];
       num -= value;
     }
   }
+
+  // Add remaining Roman numerals from obj
+  while (num !== 0) {
+    for (let i in obj) {
+      let symbol = obj[i][0];
+      let value = obj[i][1];
+
+      if (value <= num) {
+        ans += symbol;
+        num -= value;
+        break;
+      }
+    }
+  }
+
   return ans;
 }
+
