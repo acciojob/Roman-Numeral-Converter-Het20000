8ee0426a5735a06fe0c9e05b88e2f07a577a2b83
Het20000 <hp985383@gmail.com>
2025-05-12 14:16:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function convertToRoman(num) {
-  const obj = {
+  const obj = [
     0: ['M', 1000],
     1: ['D', 500],
     2: ['C', 100],
@@ -7,17 +7,15 @@ function convertToRoman(num) {
     4: ['X', 10],
     5: ['V', 5],
     6: ['I', 1]
-  };
-
+  ];
 	let ans = "";
-	while (num !==0){
-     for (let i in romanMap) {
-		let [symbol, value] = romanMap[i];
-		if (value <= num) {
-			ans += symbol;
-			num -= value;
-			break;
-		}
-	}
-}
 
+  for (const [symbol, value] of romanMap) {
+    while (num >= value) {
+      ans += symbol;
+      num -= value;
+    }
+  }
+
+  return ans;
+}
